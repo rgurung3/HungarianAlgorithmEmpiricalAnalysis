107b6f0c211e4b8e098c26102e4fcff9043e014e
Rakshak <rakshak@r440.maas>
2025-07-29 08:04:13 +0000

    THe class that is going to help store the reults so that I can print them out at the final implementation

---
+++ JavaAnalysis/src/AssignmentBreakdown.java
@@ -0,0 +1,8 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class AssignmentBreakdown {
+    public List<int[]> trueMatches = new ArrayList<>();
+    public List<int[]> falsePositives = new ArrayList<>();
+    public List<int[]> falseNegatives = new ArrayList<>();  
+}
